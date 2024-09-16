b26b797eb3050f07f2c73b19ea3c260e47c6a498
orlandopdj <orlandopdjr6@gmail.com>
2024-09-15 23:36:46 -0300

    ✅ Resolvido: 05 Accordion

--- src/app.jsx
+++ src/app.jsx
@@ -1,51 +1,37 @@
-const Avatar = () => (
-    <div>
-        <img className='avatar' src='https://media.licdn.com/dms/image/v2/D4D03AQEHlqqMHx1_EQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724376516964?e=1731542400&v=beta&t=EPU2BBfb9iELS9eo5amhJnM07jfvbjgjWVMCKDGVjeE'
-            alt="Avatar perfil" />
-
-    </div>
-)
-
-const Intro = () => (
-    <div className='info'>
-        <h1>Orlando Junior</h1>
-        <p>Sou um estudante de programação, onde busco a migração da carreira, atualmente trabalho em uma indústria de calçados, mas gostaria de trabalhar na área de tecnologia</p>
-    </div>
-)
-
-const skill = [
-    { id: crypto.randomUUID(), name: 'estudante', emoji: '🤟', bgColor: 'bg-lightblue' },
-    { id: crypto.randomUUID(), name: 'inglês', emoji: '🤏', bgColor: 'bg-yellow' },
-    { id: crypto.randomUUID(), name: 'culinária', emoji: '🤌', bgColor: 'bg-pink' },
-    { id: crypto.randomUUID(), name: 'conversar pouco', emoji: '👎', bgColor: 'bg-red' },
+const faqs = [
+    {
+        id: crypto.randomUUID(),
+        title: "Quem é a maior celebridade do mundo dos games no BR?",
+        answer: "A apresentadora Ana Maria Braga. Joga 3h pra desocupar a cabeça.",
+    },
+    {
+        id: crypto.randomUUID(),
+        title: "Onde fica a europa da américa do sul?",
+        answer: "Argentina. BR vive como rei em Buenos Aires.",
+    },
+    {
+        id: crypto.randomUUID(),
+        title: "Qual é a grande oportunidade para desenvolvedores BR?",
+        answer: "Aproveitar a alta demanda pra ganhar em dólar ou euro.",
+    },
 ]
 
-const Skill = ({ name, emoji, bgColor }) => (
-    <li className={`skill ${bgColor}`}>
-        {emoji}{name}
+const Item = ({ title, answer, i }) => (
+    <li className="item">
+        <p className="number">{i + 1}</p>
+        <h2 className="title">{title}</h2>
+        <p className="icon">-</p>
+        <div className="content-box">{answer}</div>
     </li>
 )
 
-
-const ListOfSkills = () => (
-    <ul className='list-of-skills'>
-        {skill.map(({ id, name, emoji, bgColor }) => (
-            <Skill key={id} name={name} emoji={emoji} bgColor={bgColor} />
+const App = () => (
+    <ul className="accordion">
+        {faqs.map((faq, i) => (
+            <Item key={faq.id} title={faq.title} answer={faq.answer} i={i} />
         ))}
     </ul>
-)
 
-const App = () => (
-    <>
-        <div className="card">
-            <Avatar />
-
-            <div className="info">
-                <Intro />
-                <ListOfSkills />
-            </div>
-        </div>
-    </>
 )
 
 export { App }
