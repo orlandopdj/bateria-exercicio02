5c3b8e9ae14a12362cc72ba6e6a63cf8fcc09196
orlandopdj <orlandopdjr6@gmail.com>
2024-09-15 22:57:20 -0300

    ✅ Resolvido: 04 Card

--- src/app.jsx
+++ src/app.jsx
@@ -1,50 +1,51 @@
-const cardsInfo = [
-    {
-        id: 7336,
-        question: "Do quê aplicações React são feitas?",
-        answer: "Componentes",
-    },
-    {
-        id: 8832,
-        question: "Qual é o nome da sintaxe usada para descrever UI no React?",
-        answer: "JSX",
-    },
-    {
-        id: 3457,
-        question: "Em qual linguagem o React é baseado?",
-        answer: "JavaScript",
-    },
-    {
-        id: 9103,
-        question: "Qual método usar para renderizar uma lista no React?",
-        answer: "map",
-    },
-    {
-        id: 1297,
-        question: "Qual empresa criou o React?",
-        answer: "Meta",
-    },
-    {
-        id: 2002,
-        question: "Como passar informações para um componente React?",
-        answer: "Através de props",
-    },
+const Avatar = () => (
+    <div>
+        <img className='avatar' src='https://media.licdn.com/dms/image/v2/D4D03AQEHlqqMHx1_EQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1724376516964?e=1731542400&v=beta&t=EPU2BBfb9iELS9eo5amhJnM07jfvbjgjWVMCKDGVjeE'
+            alt="Avatar perfil" />
+
+    </div>
+)
+
+const Intro = () => (
+    <div className='info'>
+        <h1>Orlando Junior</h1>
+        <p>Sou um estudante de programação, onde busco a migração da carreira, atualmente trabalho em uma indústria de calçados, mas gostaria de trabalhar na área de tecnologia</p>
+    </div>
+)
+
+const skill = [
+    { id: crypto.randomUUID(), name: 'estudante', emoji: '🤟', bgColor: 'bg-lightblue' },
+    { id: crypto.randomUUID(), name: 'inglês', emoji: '🤏', bgColor: 'bg-yellow' },
+    { id: crypto.randomUUID(), name: 'culinária', emoji: '🤌', bgColor: 'bg-pink' },
+    { id: crypto.randomUUID(), name: 'conversar pouco', emoji: '👎', bgColor: 'bg-red' },
 ]
 
+const Skill = ({ name, emoji, bgColor }) => (
+    <li className={`skill ${bgColor}`}>
+        {emoji}{name}
+    </li>
+)
 
-const App = () => {
-    const handleLog = (id, answer) => console.log(`id: ${id}, Resposta: ${answer}`)
 
-    return (
-        <ul className="flashcards">
-            {cardsInfo.map(({ id, question, answer }) => (
-                <li key={id} className="card" onClick={() => handleLog(id, answer)}>
-                    {question}
-                </li>
-            ))}
-        </ul>
-    )
+const ListOfSkills = () => (
+    <ul className='list-of-skills'>
+        {skill.map(({ id, name, emoji, bgColor }) => (
+            <Skill key={id} name={name} emoji={emoji} bgColor={bgColor} />
+        ))}
+    </ul>
+)
 
-}
+const App = () => (
+    <>
+        <div className="card">
+            <Avatar />
+
+            <div className="info">
+                <Intro />
+                <ListOfSkills />
+            </div>
+        </div>
+    </>
+)
 
 export { App }
