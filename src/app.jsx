7495adc8bc68b2ca6a57e6cb2d4e495a9d639d10
orlandopdj <orlandopdjr6@gmail.com>
2024-09-15 21:05:46 -0300

    ✅ Resolvido: 01 Contador de Data

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,25 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+const App = () => {
+    const handleClickMenosIntervalo = () => console.log('Clicou no menos do Intervalo')
+    const handleClickMenosContagem = () => console.log('Clicou no menos do Contagem')
+    const handleClickMaisIntervalo = () => console.log('Clicou no mais do Intervalo')
+    const handleClickMaisContagem = () => console.log('Clicou no mais do Contagem')
+    return (
+        <>
+            <div className="container">
+                <div className="count">
+                    <button onClick={handleClickMenosIntervalo}>-</button>
+                    <h2>Intervalo: 1</h2>
+                    <button onClick={handleClickMaisContagem}>+</button>
+                </div>
+                <div className="count">
+                    <button onClick={handleClickMenosContagem}>-</button>
+                    <h2>Contagem: 0</h2>
+                    <button onClick={handleClickMaisIntervalo}>+</button>
+                </div>
+                <h2>Hoje é Domingo, 22 de out. de 2023</h2>
+            </div>
+        </>
+    )
+}
 
-export  {App}
+export { App }
